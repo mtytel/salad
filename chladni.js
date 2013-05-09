7ab719d3913be3287c610c166ec6482b18650052
Matt Tytel <matthewtytel@gmail.com>
2013-05-09 00:41:50 -0400

    Moved some things around for efficiency.

--- chladni.js
+++ chladni.js
@@ -1,3 +1,12 @@
+/**
+ * Salad: javascript waves, resonance and Chladni figures.
+ *
+ * Copyright (c) 2013
+ * Under MIT and GPL licenses:
+ *  http://www.opensource.org/licenses/mit-license.php
+ *  http://www.gnu.org/licenses/gpl.html
+ */
+
 var chladni = function() {
   // Damping consistently puts friction on all waving points.
   var DAMPING = 1.0;
@@ -7,6 +16,7 @@ var chladni = function() {
   // Store the width and height so we don't have to access the DOM later.
   var canvas_width = 0, canvas_height = 0;
   var context = null;
+  var image = null;
   var pos_matrix = null;
   var vel_matrix = null;
   var temp_matrix = null;
@@ -26,6 +36,7 @@ var chladni = function() {
   }
 
   function tick() {
+    // Update the input equation.
     var val = 5.0 * Math.sin(time / 8.0);
     for (var i = 0; i < canvas_width; i++) {
       pos_matrix[0][i] = val;
@@ -37,9 +48,9 @@ var chladni = function() {
     }
     time++;
 
+    // Wave equation! Give an acceleration based on neighboring points.
     for (var r = 1; r < canvas_height - 1; r++) {
       for (var c = 1; c < canvas_width - 1; c++) {
-        // Wave equation! Give an acceleration based on neighboring points.
         var neighbor_sum = pos_matrix[r - 1][c] + pos_matrix[r + 1][c] +
                            pos_matrix[r][c - 1] + pos_matrix[r][c + 1];
         var difference = pos_matrix[r][c] - neighbor_sum / 4.0;
@@ -59,17 +70,8 @@ var chladni = function() {
 
         // Update the position for each point.
         pos_matrix[r][c] += vel_matrix[r][c];
-      }
-    }
-  }
 
-  function draw() {
-    // Probably shouldn't tick from draw...
-    tick();
-    var image = context.createImageData(canvas_width, canvas_height);
-
-    for (var r = 0; r < canvas_height; r++) {
-      for (var c = 0; c < canvas_width; c++) {
+        // Update the image so it's ready to draw if we need.
         var bright = Math.floor(pos_matrix[r][c] * 20);
         var index = 4 * (r * canvas_width + c);
         image.data[index] = bright;
@@ -78,8 +80,11 @@ var chladni = function() {
         image.data[index + 3] = 255;
       }
     }
+  }
 
+  function draw() {
     context.putImageData(image, 0, 0);
+    window.requestAnimationFrame(draw);
   }
 
   function init() {
@@ -87,12 +92,14 @@ var chladni = function() {
     context = canvas.getContext('2d');
     canvas_width = canvas.width;
     canvas_height = canvas.height;
+    image = context.createImageData(canvas_width, canvas_height);
     pos_matrix = createTwoDimArray(canvas_width, canvas_height);
     vel_matrix = createTwoDimArray(canvas_width, canvas_height);
     temp_matrix = createTwoDimArray(canvas_width, canvas_height);
     time = 0;
 
-    setInterval(draw, 20);
+    setInterval(tick, 20);
+    draw();
   }
 
   return {
