9ae7e2da8a22fc88fc64788b49d553e65e8a8557
Noura <culurienneldoreth@gmail.com>
2013-05-14 17:33:41 -0400

    fixed slider bounds

--- js/sliders.js
+++ js/sliders.js
@@ -5,7 +5,7 @@
  *
  * Adapted only a little by adding options and making it a JQuery function.
  *   $my_slider = $(selector).slider(options)
- *   $my_slider.setPosition(val);
+ *   $my_slider.setSliderPosition(val);
  */
 
 (function($, window) {
@@ -19,14 +19,21 @@
         step: null,
       }, o);
 
+      function actual_height($this) {
+        return parseFloat($this.css('height')) + parseFloat($this.css('padding-top')) + parseFloat($this.css('padding-bottom')) + parseFloat($this.css('border-top-width')) + parseFloat($this.css('border-bottom-width'));
+      }
+
       function on_drag(e, ui) {
         // 'this' is the slider handle
         if (!this.par) {
-          this.par = $(this).parent();
-          this.parHeight = this.par.height();
-          this.height = $(this).height();
+          var $this = $(this);
+          this.par = $this.parent();
+          // min and max possible for ui.position.top
+          // this is just how jQueryUI draggable works
+          this.min_pos = 1;
+          this.max_pos = this.par.height() - actual_height($this) + 1;
         }
-        var ratio = 1 - (ui.position.top + this.height) / this.parHeight;
+        var ratio = 1 - (ui.position.top - this.min_pos) / (this.max_pos - this.min_pos);
         var val = options.min + ratio * (options.max - options.min);
         if (options.step) {
           val = options.min + Math.floor((val - options.min) / options.step) * options.step;
@@ -40,9 +47,13 @@
           return;
         }
         var $this = $(this);
-        var ratio = (val - options.min) / (options.max - options.min);
         var $handle = $this.find('.slider-handle').first();
-        $handle.css({top: (1 - ratio) * $this.height() - $handle.height()});
+        var min_pos = 1;
+        var max_pos = $this.height() - actual_height($handle) + 1;
+
+        var ratio = 1 - (val - options.min) / (options.max - options.min);
+        var pos = min_pos + ratio * (max_pos - min_pos);
+        $handle.css({top: pos});
         $this.trigger('change', val);
       }
 
@@ -53,8 +64,8 @@
           axis:'y',
           drag: on_drag,
         });
-        $this.setPosition = set_position;
-        $this.setPosition(options.start);
+        $this.setSliderPosition = set_position;
+        $this.setSliderPosition(options.start);
       });
     }
 
